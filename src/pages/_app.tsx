7463bfdb29c7fa81c8b4e748db625704674871da
彭鐘賢 <josephpeng1014@gmail.com>
2022-03-24 16:10:21 +0800

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { Toaster } from "react-hot-toast";
 import type { AppProps } from "next/app";
 
 import "../client/styles/globals.css";
+import "react-datepicker/dist/react-datepicker.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
