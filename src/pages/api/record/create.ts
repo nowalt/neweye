7cb5ddab57b5f11d18fff712d057e831a5f6172c
彭鐘賢 <josephpeng1014@gmail.com>
2022-03-29 11:44:29 +0800

    update

--- src/pages/api/record/create.ts
+++ src/pages/api/record/create.ts
@@ -10,24 +10,30 @@ export interface Request extends NextApiRequest {
   protocol?: string;
 }
 
+interface Result {
+  type?: string;
+  count?: number;
+  action?: string;
+}
+
 export default handler().use(async (req: Request, res: NextApiResponse) => {
-  const { eyeId, clientId, data = {} } = req.body;
+  const { eyeId, clientId, data = {}, results = [] } = req.body;
 
-  // // 檢查 req.user
-  // if (!req.user || !req.user.id) {
-  //   return res.status(500).json({ error: "請先登入" });
-  // }
+  // 檢查 req.user
+  if (!req.user || !req.user.id) {
+    return res.status(500).json({ error: "請先登入" });
+  }
 
-  // // 找到登入用戶
-  // const user = await prisma.user.findUnique({
-  //   where: {
-  //     id: req.user.id,
-  //   },
-  // });
+  // 找到登入用戶
+  const user = await prisma.user.findUnique({
+    where: {
+      id: req.user.id,
+    },
+  });
 
-  // if (!user) {
-  //   return res.status(500).json({ error: "沒有找到登入用戶" });
-  // }
+  if (!user) {
+    return res.status(500).json({ error: "沒有找到登入用戶" });
+  }
 
   if (!eyeId) {
     return res.status(500).json({ error: "eye id required" });
@@ -48,14 +54,19 @@ export default handler().use(async (req: Request, res: NextApiResponse) => {
     data: {
       clientId,
       data,
-      eye: {
-        connect: {
-          id: eye.id,
-        },
-      },
-      project: {
-        connect: {
-          id: eye.projectId,
+      eye: { connect: { id: eye.id } },
+      project: { connect: { id: eye.projectId } },
+      results: {
+        createMany: {
+          data: results.map((doc: Result) => {
+            return {
+              eyeId: eye.id,
+              projectId: eye.projectId,
+              type: doc.type,
+              cout: doc.count,
+              action: doc.action,
+            };
+          }),
         },
       },
     },
