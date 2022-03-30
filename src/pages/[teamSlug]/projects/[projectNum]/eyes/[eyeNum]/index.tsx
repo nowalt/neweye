40dd5157843a825243e894a7eefbe79c524e6c97
彭鐘賢 <josephpeng1014@gmail.com>
2022-03-30 16:04:25 +0800

    update

--- src/pages/[teamSlug]/projects/[projectNum]/eyes/[eyeNum]/index.tsx
+++ src/pages/[teamSlug]/projects/[projectNum]/eyes/[eyeNum]/index.tsx
@@ -1,25 +1,44 @@
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
 
-import { useUser, useProject } from "../../../../../../client/lib/hooks";
+import {
+  useUser,
+  useProject,
+  useEyeWithRecords,
+} from "../../../../../../client/lib/hooks";
 import Header from "../../../../../../client/components/Header";
 import EyeRunner from "../../../../../../client/components/EyeRunner";
 
 const ProjectTaskPage: NextPage = () => {
   const router = useRouter();
   const slug = router.query.teamSlug as string;
-  const num = router.query.projectNum as string;
+  const projectNum = router.query.projectNum as string;
+  const eyeNum = router.query.eyeNum as string;
 
   // 防止未登入的人
   useUser();
 
-  const { project, error: projectError } = useProject({ num, teamSlug: slug });
+  const { project, error: projectError } = useProject({
+    num: projectNum,
+    teamSlug: slug,
+  });
+
+  const { eye, error: eyeError } = useEyeWithRecords({
+    projectId: project?.id,
+    num: eyeNum,
+    skip: 0,
+    take: 20,
+  });
 
   if (projectError) {
     return <div>Error: {projectError.info || projectError.message}</div>;
   }
 
-  if (!project) {
+  if (eyeError) {
+    return <div>Error: {eyeError.info || eyeError.message}</div>;
+  }
+
+  if (!project || !eye) {
     return null;
   }
 
