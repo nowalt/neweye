e376f6be94282dde14af03d3ae3bb0270f002497
彭鐘賢 <josephpeng1014@gmail.com>
2022-04-01 15:46:15 +0800

    update

--- src/pages/[teamSlug]/projects/[projectNum]/eyes/[eyeNum]/index.tsx
+++ src/pages/[teamSlug]/projects/[projectNum]/eyes/[eyeNum]/index.tsx
@@ -231,19 +231,6 @@ const ProjectTaskPage: NextPage = () => {
               )}
             </ul>
           </div>
-          {/* {hasNextPage && (
-            <div className="flex justify-center mt-3">
-              <button
-                type="button"
-                className="order-0 inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                onClick={() => {
-                  setSize(size + 1);
-                }}
-              >
-                更多記錄
-              </button>
-            </div>
-          )} */}
         </div>
       </div>
     </div>
